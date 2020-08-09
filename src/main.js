60f30a1f70356900b1c6f1c4b755b220037d3d08
Ruslan <kros8925@gmail.com>
2020-08-09 13:19:07 +0300

    Add apollo configuration file

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import { createProvider } from './vue-apollo'
 import './assets/styles/index.css'
 
 Vue.config.productionTip = false
@@ -9,5 +10,6 @@ Vue.config.productionTip = false
 new Vue({
   router,
   store,
+  apolloProvider: createProvider(),
   render: h => h(App)
 }).$mount('#app')
