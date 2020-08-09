af2f73c362c71b325091fa663efeb6dabd536590
Ruslan <kros8925@gmail.com>
2020-08-09 13:26:39 +0300

    Add GraphQL and pass it in AirlineCompanyCard

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,26 +3,37 @@
     <div class="container mx-auto">
       <div class="mb-8 text-center">
         <h1 class="mb-4 text-3xl font-bold">AirlinesReview</h1>
-        <p class="">Share your personam experience of any airline</p>
+        <p class="">Share your personal experience of any airline</p>
       </div>
 
-      <div class="grid gap-8 md:grid-cols-2 lg:grid-cols-3">
-        <AirlineCompanyCard/>
-        <AirlineCompanyCard/>
-        <AirlineCompanyCard/>
-        <AirlineCompanyCard/>
-        <AirlineCompanyCard/>
+      <section class="grid gap-8 md:grid-cols-2 lg:grid-cols-3">
+        <div v-for="airline in airlines" :key="airline.id">
+          <AirlineCompanyCard :airline="airline"/>
+        </div>
+      </section>
 
-      </div>
     </div>
   </main>
 </template>
 
 <script>
+import gql from 'graphql-tag';
+
 import AirlineCompanyCard from '@/components/AirlineCompanyCard';
 
 export default {
   name: "Home",
+
+  apollo: {
+    airlines: gql`query {
+      airlines{
+        name
+        imageUrl
+        slug
+        averageScore
+      }
+    }`
+  },
 
   components: {
     AirlineCompanyCard
