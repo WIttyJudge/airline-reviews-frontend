09092e061cfa7960aef51d073ab1f33da530531a
Ruslan <kros8925@gmail.com>
2020-08-09 13:25:43 +0300

    Add new page with reviews of airline

---
+++ src/views/Airline/Reviews.vue
@@ -0,0 +1,24 @@
+<template>
+<main class="container mx-auto">
+  <h1>{{ slug }}</h1>
+  <h1>{{ airline.averageScore }}</h1>
+</main>
+</template>
+
+<script>
+import gql from 'graphql-tag';
+
+export default {
+  props: ['slug'],
+
+  apollo: {
+    airline: gql`
+      query {
+        airline(slug: "third-airline") {
+          name
+          averageScore
+        }
+      }`
+  }
+}
+</script>
